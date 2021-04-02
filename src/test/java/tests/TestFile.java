11c5a0d3d93f13ca32660384fae5d2ab4289e542
Stessy <wildorhid@gmail.com>
2021-04-03 00:07:14 +0300

    This commit consist of 3 commits now

--- src/test/java/tests/TestFile.java
+++ src/test/java/tests/TestFile.java
@@ -2,13 +2,18 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestFile {
     @Test
     void testFile() {
         open("https://github.com/StessyT/git_hub_commits_6");
-
-        System.out.println("Hello, world");
+        $(".UnderlineNav-body").shouldHave(text("Code"));
+        $(byText("Code")).click();
+        $(".ml-md-3").shouldHave(text("1 commit"));
     }
 }
